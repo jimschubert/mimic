3ac192d774cba575724637cc66b115c801074cf9
Jim Schubert <james.schubert@gmail.com>
2022-11-23 23:22:02 -0500

    fix: viewer no longer fails if either option not set

--- view.go
+++ view.go
@@ -6,26 +6,29 @@ import (
 	"github.com/jimschubert/stripansi"
 )
 
+// Viewer is a utility for providing a String function on a mimic value.
+// This is intentionally separated from mimic.Mimic to allow for multiple outputs
+// for a single mimic, and to remove any confusion about what String might refer to.
 type Viewer struct {
 	Mimic     *Mimic
 	StripAnsi bool
 	Trim      bool
 }
 
+// String provides the full underlying dump of the terminal's view.
 func (v *Viewer) String() string {
 	if v.Mimic == nil {
 		return ""
 	}
 
-	var altered string
-	original := v.Mimic.terminal.String()
+	result := v.Mimic.terminal.String()
 	if v.Trim {
-		altered = strings.TrimSpace(original)
+		result = strings.TrimSpace(result)
 	}
 
 	if v.StripAnsi {
-		altered = stripansi.String(altered)
+		result = stripansi.String(result)
 	}
 
-	return altered
+	return result
 }
